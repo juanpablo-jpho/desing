ce843a0a3997a57ce9bffe753927d644da9ca3f1
Juan Pablo Hurtado <juanpablo.jpho@gmail.com>
2024-05-25 21:39:41 -0500

    dark mode - modo oscuro con ionic

--- src/app/shared/components/sidemenu/sidemenu.component.ts
+++ src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { IonContent, IonHeader, IonMenuToggle, IonTitle, 
          IonToolbar, IonIcon, IonButtons, IonButton, IonLabel, 
-         IonItem, IonRouterLink, MenuController } from "@ionic/angular/standalone";
+         IonItem, IonRouterLink, MenuController, 
+         IonToggle} from "@ionic/angular/standalone";
 import { Models } from 'src/app/models/models';
 import { UserService } from '../../../services/user.service';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidemenu',
@@ -18,15 +20,19 @@ import { CommonModule } from '@angular/common';
     IonMenuToggle,
     RouterModule,
     CommonModule,
-    IonRouterLink
+    IonRouterLink,
+    IonToggle,
+    FormsModule
   ]
 })
 export class SidemenuComponent  implements OnInit {
 
   menu: Menu[] = [];
+  paletteToggle = false;
 
   constructor(private menuController: MenuController) { 
       this.initMenu();
+      this.initDarkMode();
   }
 
   ngOnInit() {}
@@ -71,6 +77,35 @@ export class SidemenuComponent  implements OnInit {
       this.menuController.close('sidemenu');
     }
   }
+
+
+  initDarkMode() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    // Initialize the dark palette based on the initial
+    // value of the prefers-color-scheme media query
+    this.initializeDarkPalette(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+    // Check/uncheck the toggle and update the palette based on isDark
+    initializeDarkPalette(isDark: any) {
+      this.paletteToggle = isDark;
+      this.toggleDarkPalette(isDark);
+    }
+  
+    // Listen for the toggle check/uncheck to toggle the dark palette
+    toggleChange(ev: any) {
+      this.toggleDarkPalette(ev.detail.checked);
+    }
+  
+    // Add or remove the "ion-palette-dark" class on the html element
+    toggleDarkPalette(shouldAdd: boolean) {
+      document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+    }
+
 
 }
 
